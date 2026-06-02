1e1ba5d2a461a900aa6e275f6a5d2287bc8bfcaf
aparna65436 <aparna65436@gmail.com>
2026-06-02 11:18:54 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (54.78%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-        vector<int>temp;
-        //add all non zero element
+        int insertpos=0;
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                temp.push_back(nums[i]);
+                nums[insertpos]=nums[i];
+                insertpos++;
             }
         }
-        // add all zeroes at end of temp
-        int zeroes=n-temp.size();
-        for(int j=0;j<zeroes;j++){
-            temp.push_back(0);
+        while(insertpos<n){
+            nums[insertpos]=0;
+            insertpos++;
         }
-        // original array change
-        for(int k=0;k<n;k++){
-            nums[k]=temp[k];
-        }
+        
     }
        
 };
