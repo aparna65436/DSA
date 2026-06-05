4294a62d4dd9def13d9c750ed781b3aff1a5fef0
aparna65436 <aparna65436@gmail.com>
2026-06-05 18:09:07 +0530

    ReverseString.cpp

---
+++ Strings/ReverseString.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        char t;
+        int left=0;
+        int right=s.size()-1;
+        while(left<right){
+            swap(s[left],s[right]);
+            left++;
+            right--;
+        }
+      
+    }
+};
