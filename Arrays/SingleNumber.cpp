73c6241e622a5b0d9c594797055ac6983f14f937
aparna65436 <aparna65436@gmail.com>
2026-06-05 10:45:10 +0530

    SingleNumber.cpp

---
+++ Arrays/SingleNumber.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+//Optimal Solution
+    int singleNumber(vector<int>& nums) {
+        int XOR=0;
+        for(int i=0;i<nums.size();i++){
+            XOR=XOR^nums[i];
+        }
+        return XOR;
+       
+    }
+};
+//Better Solution
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return {};
+    }
+};
