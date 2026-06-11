2cfaa3cce5147dc94c26618e8d0a4bfb2d1f2fdc
aparna65436 <aparna65436@gmail.com>
2026-06-11 10:00:10 +0530

    Permutations.cpp

--- Backtracking/Permutations.cpp
+++ Backtracking/Permutations.cpp
@@ -5,14 +5,12 @@ public:
             ans.push_back(nums);
             return;
         }
-
         for(int i = index; i < nums.size(); i++) {
             swap(nums[index], nums[i]);      // choose
             solve(nums, ans, index + 1);     // explore
             swap(nums[index], nums[i]);      // backtrack
         }
     }
-
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         solve(nums, ans, 0);
