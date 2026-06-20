2b2b1480afe9cc21a7a430f1bc642e59242796a2
aparna65436 <aparna65436@gmail.com>
2026-06-20 12:45:20 +0530

    NextGreaterElement.cpp

--- Stack/NextGreaterElement.cpp
+++ Stack/NextGreaterElement.cpp
@@ -1,3 +1,36 @@
+//Optimal Approach
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> mp;
+        stack<int> st;
+
+        // Step 1: Process nums2
+        for(int i = nums2.size() - 1; i >= 0; i--) {
+            while(!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if(st.empty()) {
+                mp[nums2[i]] = -1;
+            } else {
+                mp[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        // Step 2: Build answer for nums1
+        vector<int> ans;
+        for(int i = 0; i < nums1.size(); i++) {
+            ans.push_back(mp[nums1[i]]);
+        }
+
+        return ans;
+    }
+};
+
+//Brute Force Approach
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
