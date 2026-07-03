b48a7bce6a308f0188d58fa2664a84c3c854b269
aparna65436 <aparna65436@gmail.com>
2026-07-03 10:50:59 +0530

    CountCompleteTree.cpp

---
+++ Trees/CountCompleteTree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) { 
+        queue<TreeNode* >q;
+        if(root==NULL){
+            return 0;
+        }
+        q.push(root);
+        int n=q.size();
+        int c=0;
+        while(!q.empty()){
+            TreeNode* temp=q.front();
+            q.pop();
+            c++;
+            for(int i=0;i<n;i++){
+               if(temp->left!=NULL){
+                q.push(temp->left);
+               }
+               if(temp->right!=NULL){
+                q.push(temp->right);
+               }
+            } 
+        }
+        return c;
+        
+    }
+};
