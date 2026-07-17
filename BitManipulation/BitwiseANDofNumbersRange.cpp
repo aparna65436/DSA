b9a40c7ebaf31b69a02cfa7f1c085dd540e93a97
aparna65436 <aparna65436@gmail.com>
2026-07-17 18:41:02 +0530

    BitwiseANDofNumbersRange.cpp

--- BitManipulation/BitwiseANDofNumbersRange.cpp
+++ BitManipulation/BitwiseANDofNumbersRange.cpp
@@ -1,3 +1,14 @@
+//Optimal Sol
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        while(left<right){
+            right=right&(right-1);
+        }
+        return left&right;
+    }
+};
+//Brute Force Approach
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
