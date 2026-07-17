4603ead35331f58b884c11d060795cd6495ddbac
aparna65436 <aparna65436@gmail.com>
2026-07-17 12:20:35 +0530

    MissingNumbers.cpp

--- Arrays/MissingNumbers.cpp
+++ Arrays/MissingNumbers.cpp
@@ -1,3 +1,25 @@
+//Brute Force Approach
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        for(int i=0;i<=nums.size();i++){
+            int c=0;
+            for(int j=0;j<nums.size();j++){
+                if(i==nums[j]){
+                    c=1;
+                }
+            }
+            if(c!=1){
+                return i;
+            }
+
+        }
+        return -1;
+        
+    }
+};
+
+
 class Solution {
 public:
  int missingNumber(vector<int>& nums) {
