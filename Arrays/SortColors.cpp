469158d4252dddd05241c911f606fd159d61cb36
aparna65436 <aparna65436@gmail.com>
2026-09-06 22:57:21 +0530

    SortColors.cpp

--- Arrays/SortColors.cpp
+++ Arrays/SortColors.cpp
@@ -21,3 +21,12 @@ public:
        }
     }
 };
+#SORT COLORS
+
+##Approach Used Dutch National Flag Algorithm with three pointer
+
+-start for 0 -mid for current element -end for 2
+
+#Time Complexity O(n)
+
+#Space Complexity O(1)
