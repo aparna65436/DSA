12fe215b91ec024efb9eab165736c5674cdefdd8
aparna65436 <aparna65436@gmail.com>
2026-09-09 14:29:12 +0530

    RotateImage.cpp

---
+++ Arrays/RotateImage.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix.size();j++){
+                if(i>j){
+                    swap(matrix[i][j],matrix[j][i]);
+                }
+            }
+
+        }
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix.size();j++){
+                reverse(matrix[i][j]);
+            }
+        }
+    }
+};
